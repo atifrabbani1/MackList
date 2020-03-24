1f3a17b64b624144d3cd59c7dace5df479f892b0
Khiry Hill <khiryhill7@gmail.com>
2020-03-23 23:37:17 -0400

    added photo

--- routes/api/images.js
+++ routes/api/images.js
@@ -9,59 +9,60 @@ const uploadDir = path.join(process.cwd(), "client/src/uploads")
 
 // Multer this will store the image in memory and upload the image into the uplaod folder 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: function (req, file, cb) {
         cb(null, uploadDir);
     },
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         console.log(file)
-        cb(null, uuid() +"__"+ file.originalname);
+        cb(null, uuid() + "__" + file.originalname);
     }
- });
+});
 
-  
-  // this function will reject any file that is not either a jpeg or png
-  const fileFilter = (req, file, cb) => {
-      console.log("##################### ")
+
+// this function will reject any file that is not either a jpeg or png
+const fileFilter = (req, file, cb) => {
+    console.log("##################### ")
     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
         cb(null, true);
     } else {
         cb(null, false)
     }
-  };
-  
+};
+
 const upload = multer({
     storage: storage,
-    fileFilter:fileFilter
-}); 
+    fileFilter: fileFilter
+});
 
 // adRouter.route("../client/src/pages/user-post")
-router.post("/", upload.single("productImage"), (req, res, next)=>{
+router.post("/", upload.single("productImage"), (req, res, next) => {
     console.log(req.file);
     db.Ad.create({
-        owner: req.body.owner ,
-        productTitle:req.body.productTitle,
-       // productImage: path.join( uploadDir, req.file.filename) ,
+        owner: req.body.owner,
+        productTitle: req.body.productTitle,
+        // productImage: path.join( uploadDir, req.file.filename) ,
         productImage: req.file.filename,
-        productDescription:req.body.productDescription ,
-        locationState:req.body.locationState ,
+        productDescription: req.body.productDescription,
+        locationState: req.body.locationState,
         sellerContactPhone: req.body.sellerContactPhone,
         sellerContactEmail: req.body.sellerContactEmail,
     })
-    // const newAd = new Ad({
-    //     owner: req.body.owner ,
-    //     productTitle:req.body.productTitle,
-    //     productImage: path.join( uploadDir, req.file.filename) ,
-    //     productDescription:req.body.productDescription ,
-    //     productDepartment: req.body.productDepartment,
-    //     productCost:req.body.productCost ,
-    //     locationCity:req.body.locationCity ,
-    //     locationState:req.body.locationState ,
-    //     sellerContactName:req.body.sellerContactName ,
-    //     sellerContactPhone: req.body.sellerContactPhone,
-    //     sellerContactEmail: req.body.sellerContactEmail,
-    // })
-    // newAd.save()
-    .then((results)=>{
-        console.log(res);
+        // const newAd = new Ad({
+        //     owner: req.body.owner ,
+        //     productTitle:req.body.productTitle,
+        //     productImage: path.join( uploadDir, req.file.filename) ,
+        //     productDescription:req.body.productDescription ,
+        //     productDepartment: req.body.productDepartment,
+        //     productCost:req.body.productCost ,
+        //     locationCity:req.body.locationCity ,
+        //     locationState:req.body.locationState ,
+        //     sellerContactName:req.body.sellerContactName ,
+        //     sellerContactPhone: req.body.sellerContactPhone,
+        //     sellerContactEmail: req.body.sellerContactEmail,
+        // })
+        // newAd.save()
+        .then((results) => {
+            console.log(res);
+        });
 })
-  module.exports= router;
+module.exports = router;
